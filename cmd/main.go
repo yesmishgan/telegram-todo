7082c1da4578d96a6790bf95f141ee25374ff75e
Misha Dolbnin <dolbnin.mihail@yandex.ru>
2021-09-19 23:48:45 +0300

    Added preparatory activities

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,31 @@ package main
 
 import (
 	"fmt"
-
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"os"
 )
 
 func main() {
-
+	logger := zap.NewExample() //Change to zap.NewProduction()
 	if err := InitConfig(); err != nil {
-		zap.NewPro
+		logger.Fatal(fmt.Sprintf("error initializing configs: %s", err.Error()))
 	}
-	fmt.Println("Test")
+
+	if err := godotenv.Load(); err != nil {
+		logger.Fatal(fmt.Sprintf("error loading env variables: %s", err.Error()))
+	}
+
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TG_TOKEN"))
+	if err != nil {
+		logger.Fatal(fmt.Sprintf("error initializing tg token: %s", err.Error()))
+	}
+
+	logger.Info(fmt.Sprintf("authorized on account %s", bot.Self.UserName))
+
+	bot.Debug = true
 }
 
 func InitConfig() error {
--- go.mod
+++ go.mod
@@ -1,10 +1,20 @@
-module tg_todo
+module telegram_todo
 
 go 1.16
 
 require (
-	github.com/spf13/viper v1.8.1
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/mitchellh/mapstructure v1.4.2 // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/spf13/cast v1.4.1 // indirect
+	github.com/spf13/viper v1.8.1 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.7.0 // indirect
 	go.uber.org/zap v1.19.1 // indirect
+	golang.org/x/sys v0.0.0-20210917161153-d61c044b1678 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/ini.v1 v1.63.2 // indirect
 )
