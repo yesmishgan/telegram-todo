3049597dc579bcce96ab6f6af661ccce1b8f17c3
Misha Dolbnin <dolbnin.mihail@yandex.ru>
2021-09-19 23:56:06 +0300

    Added Dockerfile

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
+	"log"
 	"os"
 )
 
@@ -27,6 +28,24 @@ func main() {
 	logger.Info(fmt.Sprintf("authorized on account %s", bot.Self.UserName))
 
 	bot.Debug = true
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
 }
 
 func InitConfig() error {
