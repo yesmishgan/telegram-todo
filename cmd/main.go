d958b88fdc06f121fab2f6a7f1f17d6da79a395f
Misha Dolbnin <dolbnin.mihail@yandex.ru>
2021-09-19 22:35:16 +0300

    Add zap and viper

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/spf13/viper"
 )
 
-func main(){
+func main() {
+
+	if err := InitConfig(); err != nil {
+		zap.NewPro
+	}
 	fmt.Println("Test")
+}
+
+func InitConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
---
+++ go.mod
@@ -0,0 +1,10 @@
+module tg_todo
+
+go 1.16
+
+require (
+	github.com/spf13/viper v1.8.1
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	go.uber.org/zap v1.19.1 // indirect
+)
